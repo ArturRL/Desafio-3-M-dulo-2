b446f4a0a582f5c4636a6ba3ac6b3afee1d45e65
ArturRL <artur.rod.lopes@gmail.com>
2022-07-15 20:15:08 -0300

    Desafio entregue

---
+++ script.js
@@ -0,0 +1,19 @@
+// for e while
+
+for(let number=1; number<=10; number+1){
+    console.log(number)
+}
+
+let number2=10
+while(1<= number2){
+    console.log(number2)
+    number2=1 
+}
+
+for(let numeroimpar=1; numeroimpar<=100; numeroimpar+=2){
+    console.log(numeroimpar)
+}
+
+for(let numeropar=0; numeropar<=100; numeropar+=2){
+    console.log(numeropar)
+}
